bcc32d16d76d496321a9abbee1d12a0fb4299ed2
Jordan <tamwengjing98@gmail.com>
2018-04-03 12:17:53 +0800

    added new cpp

---
+++ p6.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int 1, 2, 3, ans;
+
+	cout << "insert number 1:" << endl;
+	cin >> 1;
+	cout << "insert number 2:" << endl;
+	cin >> 2;
+	cout <<"insert number 3:" << endl;
+	cin >>3;
+	ans = 1 * 2 * 3;
+
+	cout <<"Number 1 * Number 2 * Number 3 = " << ans << endl;
+
+	system("pause");
+}
